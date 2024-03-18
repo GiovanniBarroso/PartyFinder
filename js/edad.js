37b68523469366cb68c60b71423ba37f54d345d0
Giovanni Barroso <giovanni.baralv@gmail.com>
2024-03-18 11:24:01 +0100

    Añadido etiquetas METADATA CEO en head de index.html // Mejorado Responsive full media-querys //

--- js/edad.js
+++ js/edad.js
@@ -1,15 +1,17 @@
-"use strict"
+/*let edadUsuario = prompt("Por favor, ingresa tu edad:");
 
-let edad = alert('Esta pagina es para mayores de 18 años, pulsa el boton de aceptar si usted es mayor de edad.');
+// Verificar si el usuario puede entrar a la web
+if (edadUsuario >= 18) {
+    alert("¡Bienvenido/a a nuestra web!");
 
-function verificarEdad() {
-    var respuesta = confirm("¿Eres mayor de edad?");
-
-    if (respuesta) {
-        alert("Eres mayor de edad. Puedes continuar.");
-    } else {
-        alert("Eres menor de edad. No puedes continuar.");
+} else {
+    if (edadUsuario >= 0 && edadUsuario <= 3) {
+        window.location.href = "https://www.dodot.es/";
+    } else if (edadUsuario >= 4 && edadUsuario <= 6) {
+        window.location.href = "https://www.pocoyo.com/";
+    } else if (edadUsuario >= 7 && edadUsuario <= 12) {
+        window.location.href = "https://harrypotter.fandom.com/es/wiki/HarryPotter_Wiki";
+    } else if (edadUsuario >= 13 && edadUsuario <= 17) {
+        window.location.href = "https://www.lego.com/";
     }
-}
-
-
+}*/
